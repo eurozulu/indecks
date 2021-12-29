e732ba9493b6b75ba889480f96bf0c9d467a666c
Rob Gilham <robert.gilham@linkpizza.com>
2021-12-29 14:32:52 +0000

    remove maptools

--- indexer.go
+++ indexer.go
@@ -106,3 +106,16 @@ func (i Indexer) parseProperties(p string, by []byte) map[string]Property {
 	}
 	return props
 }
+
+func appendMap(m map[string]Property, ms map[string]Property) map[string]Property {
+	if len(ms) == 0 {
+		return m
+	}
+	if m == nil {
+		m = map[string]Property{}
+	}
+	for k, v := range ms {
+		m[k] = v
+	}
+	return m
+}
